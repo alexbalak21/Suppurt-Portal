ff9f6156b5fa784b35643d717d9f80e5014753c0
Alex Balak <alex.balak@outlook.com>
2026-01-23 21:04:11 +0100

    react dom implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css"
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import About from "./pages/About"
+import Demo from "./pages/Demo"
+
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">Issue Tracker</h1>
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Issue Tracker</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
     </div>
   )
 }
